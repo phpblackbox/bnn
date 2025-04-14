704189e4356f39e6dc358a5cdac6c296764e803c
phpblackbox <coffeeocean918@outlook.com>
2025-04-14 16:26:28 +0800

    wip: post media slider

--- lib/screens/post/post_item.dart
+++ lib/screens/post/post_item.dart
@@ -6,11 +6,10 @@ import 'package:bnn/providers/post_provider.dart';
 import 'package:bnn/providers/profile_provider.dart';
 import 'package:bnn/screens/chat/room.dart';
 import 'package:bnn/screens/home/comments.dart';
-import 'package:bnn/widgets/FullScreenVideo.dart';
 import 'package:bnn/widgets/buttons/button-post-action.dart';
 import 'package:bnn/widgets/toast.dart';
-import 'package:bnn/widgets/FullScreenImage.dart';
 import 'package:bnn/widgets/post/share_modal.dart';
+import 'package:bnn/widgets/post/PostMediaViewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_supabase_chat_core/flutter_supabase_chat_core.dart';
@@ -378,21 +377,14 @@ class _PostItemState extends State<PostItem> {
                       postProvider.getFileType(widget.post['img_urls'][index2]);
                   return GestureDetector(
                     onTap: () {
-                      if (fileType == "image") {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => FullScreenImage(
-                                imageUrl: widget.post['img_urls'][index2]!),
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PostMediaViewer(
+                            mediaUrls: widget.post['img_urls'],
+                            initialIndex: index2,
                           ),
-                        );
-                      } else if (fileType == "video") {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => FullScreenVideo(
-                                videoUrl: widget.post['img_urls'][index2]!),
-                          ),
-                        );
-                      }
+                        ),
+                      );
                     },
                     child: Container(
                       margin: EdgeInsets.symmetric(horizontal: 5.0),
